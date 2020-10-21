ca8e42dcba3cd1fa14217450614a87497ef18dcf
T-Dogg <tedtremlett@gmail.com>
2020-10-21 17:27:58 +0200

    Added ability to create new listings

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,52 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class User(AbstractUser):
     pass
+
+
+# Implement model for the item categories
+class Category(models.Model):
+    category_name = models.CharField(max_length=64, blank=False)
+
+    # When this category is displayed as a string, select what it will show up as on the site
+    def __str__(self):
+        return self.category_name
+
+
+# Implement model for the Auction Listings
+class AuctionListing(models.Model):
+    name = models.CharField(max_length=128, blank=False)  # Name of the listing is a CharField, max length allowed 128 characters
+    description = models.TextField(blank=False)  # Item description is a text field with undefined length for now
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2, blank=False)  # Starting bid with 2dp, up to 1 Million
+    current_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)  # Current price, same as above
+    image_link = models.URLField(blank=True)  # Optional field for image link URL
+    category = models.CharField(max_length=128, blank=True, null=True)  # Category will be its own class so that admin users can add mroe categories if needed
+
+    # When this listing is displayed on the site, show the name as a default
+    def __str__(self):
+        return self.name
+
+    #
+    def get_absolute_url(self):
+        return reverse('')
+
+
+# # Implement model for bids
+# class Bids(models.Model):
+#     bidder = User.get_username()  # Get the name of the user making a new bid
+#     bid = models.DecimalField(max_digits=8, decimal_places=2, blank=False)  # Store the value of the new bid
+
+
+# # Implement model for comments
+# class Comments(models.Model):
+#     commentor = User.get_username()  # Get the name of the user creating the comment
+#     listing = models.OneToOneField(AuctionListing, on_delete=models.CASCADE)
+#
+#
+# # Implement model for each user's watchlist
+# class Watchlist(models.Model):
+#     user = User.get_username()  # Get the name of the user who's Watchlist we are going to
+#     listings = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="listing")
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,66 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django import forms
+from django.db.models.fields import BLANK_CHOICE_DASH
 
-from .models import User
+from .models import User, AuctionListing, Category
+
+# Create our category choices variable based on the categories that get created in the Admin view
+category_choices = Category.objects.all().values_list('category_name', 'category_name')
+category_choices_list = []
+for item in category_choices:
+    category_choices_list.append(item)
+
+# Create Django Form for creating a new listing
+class Create(forms.ModelForm):
+    class Meta:
+        # Take fields from the AuctionListing model
+        model = AuctionListing
+        # For this form, display only these fields
+        fields = ['name', 'description', 'starting_bid', 'image_link', 'category']
+        # Use default input types for all the fields (defined in models.py), but specify that categories will be
+        # selected from the list of available categories
+        widgets = {
+            'category': forms.Select(choices=BLANK_CHOICE_DASH + category_choices_list)
+        }
 
 
+# Default view / landing page
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "active_listings": AuctionListing.objects.all()
+    })
+
+# Method for creating new listings
+def create(request):
+    # If request type is POST (i.e. the form is being submitted from the Create page), then store the submitted data
+    # in create_form
+    if request.method == "POST":
+        create_form = Create(request.POST)
+
+        # If the form is valid, create an empty row in AuctionListing and fill each applicable section from the
+        # cleaned data
+        if create_form.is_valid():
+            new_listing = AuctionListing()
+            new_listing.name = create_form.cleaned_data["name"]
+            new_listing.description = create_form.cleaned_data["description"]
+            new_listing.starting_bid = create_form.cleaned_data["starting_bid"]
+            new_listing.image_link = create_form.cleaned_data["image_link"]
+            new_listing.category = create_form.cleaned_data["category"]
+
+            new_listing.save()
+
+            return redirect('index')
+
+        return redirect('index')
+
+    else:
+        return render(request, "auctions/create.html", {
+            "create_form": Create()
+        })
 
 
 def login_view(request):
